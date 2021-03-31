469b61a4ea3cc74e9ba83e77cfda6952a66f5d7d
blizzardnya <e.hmelnikov@yandex.by>
2021-03-31 15:37:04 +0300

    Fixed LoginPage styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -16,6 +16,9 @@ import { login } from "../redux/slices/accountSlice";
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
@@ -27,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    color: "#fff",
   },
 }));
 
